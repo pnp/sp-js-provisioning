ae1dd4a0a7fc818235469ec66cecfc9a6f72baad
olemp <olemp@puzzlepart.com>
2019-11-11 16:07:30 +0100

    Supporting token {site} in replaceUrlTokens

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,12 +3,16 @@ import { IProvisioningConfig } from "../provisioningconfig";
 
 export function replaceUrlTokens(str: string, config: IProvisioningConfig): string {
     let siteAbsoluteUrl = null;
+    let siteRelativeUrl = null;
     if (config.spfxContext) {
         siteAbsoluteUrl = config.spfxContext.pageContext.site.absoluteUrl;
+        siteRelativeUrl = config.spfxContext.pageContext.site.serverRelativeUrl;
     } else if (window.hasOwnProperty("_spPageContextInfo")) {
         siteAbsoluteUrl = _spPageContextInfo.siteAbsoluteUrl;
+        siteRelativeUrl = _spPageContextInfo.siteServerRelativeUrl;
     }
     return str
+        .replace(/{site}/g, siteRelativeUrl)
         .replace(/{sitecollection}/g, siteAbsoluteUrl)
         .replace(/{wpgallery}/g, `${siteAbsoluteUrl}/_catalogs/wp`)
         .replace(/{hosturl}/g, `${window.location.protocol}//${window.location.host}:${window.location.port}`)
